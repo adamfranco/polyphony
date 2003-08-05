dfcfbe23e19a4f677f09f1ce1f1da10d98f2208b
gabeschine <gabeschine>
2003-08-05 20:59:31 +0000

    first import

--- polyphony.inc.php
+++ polyphony.inc.php
@@ -5,7 +5,7 @@
  * classes.
  *
  * @package polyphony
- * @version $Id: polyphony.inc.php,v 1.1 2003/08/05 20:59:31 gabeschine Exp $
+ * @version $Id: polyphony.inc.php,v 1.1.1.1 2003/08/05 20:59:31 gabeschine Exp $
  * @copyright 2003 
  **/
 
