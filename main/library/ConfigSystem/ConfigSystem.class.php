aa927c9c3bb0af64551227010ba44a94926a3503
gabeschine <gabeschine>
2004-07-29 03:44:03 +0000

    fixed ConfigSystem to work with new DataManager

--- main/library/ConfigSystem/ConfigSystem.class.php
+++ main/library/ConfigSystem/ConfigSystem.class.php
@@ -4,15 +4,15 @@
 *
 * @package
 * @copyright 2004
-* @version $Id: ConfigSystem.class.php,v 1.3 2004/07/22 19:36:49 gabeschine Exp $
+* @version $Id: ConfigSystem.class.php,v 1.4 2004/07/29 03:44:03 gabeschine Exp $
 */
 class ConfigSystem {
 
 	/**
-	* @variable object $_typeDef The {@link DataSetTypeDefinition} corresponding to our config setup.
+	* @variable object $_schema The {@link Schema} corresponding to our config setup.
 	* @access private
 	**/
-	var $_typeDef;
+	var $_schema;
 
 	/**
 	* @variable boolean $_setup Specifies if we are in setup mode, or "change settings" mode.
@@ -21,72 +21,64 @@ class ConfigSystem {
 	var $_setup;
 
 	/**
-	* @variable object $_dataSet The {@link DataSet} which is associated with this config system.
+	* @variable object $_record The {@link Record} which is associated with this config system.
 	* @access private
 	**/
-	var $_dataSet;
+	var $_record;
 
 	/**
-	* @variable object $_dataSetType A {@link HarmoniType} object associated with our DataSet.
+	* @variable object $_schemaType A {@link HarmoniType} object associated with our Record.
 	* @access private
 	**/
-	var $_dataSetType;
+	var $_schemaType;
 	/**
-	* @variable array $_defaults An array of {@link DataType}s describing the default values for each property.
+	* @variable array $_defaults An array of {@link Primitive}s describing the default values for each property.
 	* @access private
 	**/
 	var $_defaults;
-	/**
-	* @variable array $_defaults An array of descriptions for each property.
-	* @access private
-	**/
-	var $_descriptions;
 
 	/**
 	* The constructor.
 	* @param string $program The name of the program to setup configuration for, such as "Segue" or "Concerto".
 	**/
 	function ConfigSystem($program) {
-		// here we need to get/create a DataSetTypeDefinition for this setup
-		if (!Services::serviceAvailable("DataSetTypeManager")) {
+		// here we need to get/create a Schema for this setup
+		if (!Services::serviceAvailable("SchemaManager")) {
 			throwError(
 			new Error(
-			"ConfigSystem - could not continue because the DataSetTypeManager service is not available!","ConfigSystem",true)
+			"ConfigSystem - could not continue because the SchemaManager service is not available!","ConfigSystem",true)
 			);
 			return;
 		}
 
-		$typeManager =& Services::getService("DataSetTypeManager");
+		$typeManager =& Services::getService("SchemaManager");
 
-		$this->_dataSetType =& new HarmoniType("Polyphony","ConfigSystem",$program);
+		$this->_schemaType =& new HarmoniType("Polyphony","ConfigSystem",$program);
+		
+		$schema =& $typeManager->newSchema($this->_schemaType);
 
-		$typeDef =& $typeManager->newDataSetType($this->_dataSetType);
-
-		$this->_typeDef =& $typeDef;
+		$this->_schema =& $schema;
 
 		$this->_setup = true;
-		$this->_dataSet = null;
+		$this->_record = null;
 		$this->_defaults = array();
-		$this->_descriptions = array();
 	}
 
 	/**
 	* Adds a property with the name & type specified. This is used while setting up the config system.
-	* @param ref object $field A {@link FieldDefinition} defining the properties of this field.
-	* @param optional string $description An optional description of this property. 
+	* @param ref object $field A {@link SchemaField} defining the properties of this field.
 	* @param optional object $default The default value for this property. Must be the correct class that is associated
-	* with the $type value, as defined by the DataTypeManager.
+	* with the {@link SchemaField} type value, as defined by the DataTypeManager.
 	* @access public
 	* @return void
 	*/
-	function addProperty($field, $description = "", $default=null)
+	function addProperty($field, $default=null)
 	{
 		$typeManager =& Services::getService("DataTypeManager");
 
 		if ($default == null || $typeManager->isObjectOfDataType($default, $field->getType())) {
-			$this->_typeDef->addNewField( $field );
+			$this->_schema->addField( $field );
 			if ($default) $this->_defaults[$field->getLabel()] =& $default;
-			$this->_descriptions[$field->getLabel()] = $description;
 		}
 	}
 
@@ -97,15 +89,15 @@ class ConfigSystem {
 	*/
 	function finishSetup()
 	{
-		$manager =& Services::getService("DataSetTypeManager");
-		$manager->synchronize($this->_typeDef);
+		$manager =& Services::getService("SchemaManager");
+		$manager->synchronize($this->_schema);
 
 		// now go through and set up all our default values. but only do so if it's not already stored in the DB.
-		$dataSet =& $this->getDataSet(true);
-		if (!$dataSet->getID()) {
+		$record =& $this->getRecord(true);
+		if (!$record->getID()) {
 			foreach (array_keys($this->_defaults) as $key) {
 				if ($this->_defaults[$key])
-					$dataSet->setValue($key,$this->_defaults[$key],NEW_VALUE);
+					$record->setValue($key,$this->_defaults[$key],NEW_VALUE);
 			}
 		}
 
@@ -114,62 +106,50 @@ class ConfigSystem {
 	}
 
 	/**
-	 * Returns the string description of the named property.
-	 * @param string $key The property name.
-	 * @access public
-	 * @return string
-	 */
-	function getDescriptions($key)
-	{
-		return $this->_descriptions[$key];
-	}
-	
-	/**
 	* Saves all the config data to the database.
 	* @access public
 	* @return void
 	*/
 	function save()
 	{
-		$dataSet =& $this->getDataSet();
-		$dataSet->commit();
+		if (is_object($this->_record)) 
+			$this->_record->commit();
 	}
 
 	/**
-	* Returns a {@link DataSet} ready for editing permissions.
+	* Returns a {@link Record} ready for editing permissions.
 	* @param optional boolean $force This option is only used internally.
 	* @access public
 	* @return ref object
 	*/
-	function &getDataSet($force=false)
+	function &getRecord($force=false)
 	{
 		if ($this->_setup && !$force) {
 			throwError(
-			new Error("ConfigSystem::getDataSet() - setup must first be completed by calling finishSetup() before the DataSet will be available.","ConfigSystem",true));
+			new Error("ConfigSystem::getRecord() - setup must first be completed by calling finishSetup() before the Record will be available.","ConfigSystem",true));
 		}
 
-		if ($this->_dataSet) return $this->_dataSet;
-		$dataSetManager =& Services::getService("DataSetManager");
+		if ($this->_record) return $this->_record;
+		$recordManager =& Services::getService("RecordManager");
 
-		// first we will search the dataset manager to see if it has any datasets (hopefully only one!!) of the type
+		// first we will search the record manager to see if it has any records (hopefully only one!!) of the type
 		// that we are looking for. otherwise, we will create a new one.
-		$search = new AndSearch();
-		//		$search->addCriteria(new DataSetTypeSearch($this->_typeDef->getType()));
-		$search->addCriteria(new DataSetTypeSearch($this->_typeDef->getType()));
-		$search->addCriteria(new ActiveDataSetsSearch());
-
-		$ids = $dataSetManager->selectIDsBySearch($search);
+		$ids = $recordManager->getRecordIDsByType($this->_schemaType);
 
 		// do we have any ids?
 		if (count($ids)) {
 			// we'll use the first id, since there really only should be one.
 			$id = $ids[0];
-			$this->_dataSet =& $dataSetManager->fetchDataSet($id, true);
+			$this->_record =& $recordManager->fetchRecord($id, true);
+			// if for some reason the record is not active, let's activate it
+			if (!$this->_record->isActive()) {
+				$this->_record->setActiveFlag(true);
+			}
 		} else {
-			$this->_dataSet =& $dataSetManager->newDataSet($this->_dataSetType, false);
+			$this->_record =& $recordManager->newRecord($this->_schemaType, false);
 		}
 
-		return $this->_dataSet;
+		return $this->_record;
 	}
 
 }
