0cd35d15dd930be0813b46b3551cd69ec9f3ff56
adamfranco <adamfranco>
2004-11-10 14:30:17 +0000

    Added functionality to view agents and functions.

---
+++ main/modules/authorization/view_agents.act.php
@@ -0,0 +1,95 @@
+<?
+
+// Get the Layout compontents. See core/modules/moduleStructure.txt
+// for more info. 
+$harmoni->ActionHandler->execute("window", "screen");
+$mainScreen =& $harmoni->getAttachedData('mainScreen');
+$statusBar =& $harmoni->getAttachedData('statusBar');
+$centerPane =& $harmoni->getAttachedData('centerPane');
+ 
+
+// Our
+$actionRows =& new RowLayout();
+$centerPane->addComponent($actionRows, TOP, CENTER);
+
+// Intro
+$introHeader =& new SingleContentLayout(HEADING_WIDGET, 2);
+$introHeader->addComponent(new Content(_("View Agents")));
+$actionRows->addComponent($introHeader);
+
+$sharedManager =& Services::getService("Shared");
+
+// $testType =& new HarmoniType("AuthorityX", "domainX", "KeywordX", "This is a really great description");
+// $sharedManager->createAgent("That guy in camo", $testType);
+
+
+// $sharedManager =& Services::getService("Shared");
+// 
+// $qualifierHierarchyId =& $sharedManager->getId("673");
+// $functionId =& $sharedManager->createId();
+// $functionType =& new HarmoniType("Concerto", "Midd", "Use", "Functions for viewing and using shiznat.");
+// $authZManager->createFunction($functionId, "Comment", "Add comments to this thing.", $functionType, $qualifierHierarchyId);
+// exit;
+
+
+
+// Get all the agents.
+$agents =& $sharedManager->getAgents();
+while ($agents->hasNext()) {
+	$agent =& $agents->next();
+	ob_start();
+	$id =& $agent->getId();
+	print "<strong>".$id->getIdString()." - ";
+	print $agent->getDisplayName()."</strong>";
+	$agentLayout =& new SingleContentLayout(HEADING_WIDGET, 2);
+	$agentLayout->addComponent(new Content(ob_get_contents()));
+	ob_end_clean();
+	
+	// Also display type information for that agent
+	
+	ob_start();
+	$agentType =& $agent->getType();
+	print "<em>Authority: </em>".$agentType->getAuthority();
+	print "<em><br />Domain: </em>".$agentType->getDomain();
+	print "<em><br />Keyword: </em>".$agentType->getKeyword();
+	$agentInfoLayout =& new SingleContentLayout(TEXT_BLOCK_WIDGET, 2);
+	$agentInfoLayout->addComponent(new Content(ob_get_contents()));
+	ob_end_clean();
+	
+	ob_start();
+	print "<em>Description: </em>".$agentType->getDescription();
+	$agentDescriptionLayout =& new SingleContentLayout(TEXT_BLOCK_WIDGET, 1);
+	$agentDescriptionLayout->addComponent(new Content(ob_get_contents()));
+	ob_end_clean();
+		
+	$actionRows->addComponent($agentLayout);
+	$actionRows->addcomponent($agentInfoLayout);
+	$actionRows->addComponent($agentDescriptionLayout);
+	
+	$actionRows->addComponent(new Content(" &nbsp; <br /> &nbsp; <br /> &nbsp;"));
+	
+/// / 	Now get all the functions of the specified type
+// 	$functions =& $authZManager->getFunctions($functionType);
+// 	while ($functions->hasNext()) {
+// 		$function =& $functions->next();
+// 		
+// 	//	Store function info and create layouts.
+// 	//	Types consist of {domain :: authority :: type :: description}
+// 	//	Functions consist of {Id :: displayName :: description}
+// 		
+// 		ob_start();
+// 		$id =& $function->getId();
+// 		print $id->getIdString()." - ";
+// 		print "<strong>".$function->getReferenceName()." - ";
+// 		print "</strong>".$function->getDescription();
+// 		
+// 		$functionLayout->addComponent(new Content(ob_get_contents()));
+// 		ob_end_clean();
+//         
+// 	}
+}
+
+
+
+// Return the main layout.
+return $mainScreen;
---
+++ main/modules/authorization/view_functions.act.php
@@ -0,0 +1,86 @@
+<?
+
+// Get the Layout compontents. See core/modules/moduleStructure.txt
+// for more info. 
+$harmoni->ActionHandler->execute("window", "screen");
+$mainScreen =& $harmoni->getAttachedData('mainScreen');
+$statusBar =& $harmoni->getAttachedData('statusBar');
+$centerPane =& $harmoni->getAttachedData('centerPane');
+ 
+
+// Our
+$actionRows =& new RowLayout();
+$centerPane->addComponent($actionRows, TOP, CENTER);
+
+// Intro
+$introHeader =& new SingleContentLayout(HEADING_WIDGET, 2);
+$introHeader->addComponent(new Content(_("View Functions")));
+$actionRows->addComponent($introHeader);
+
+$authZManager =& Services::getService("AuthZ");
+
+
+
+
+// $sharedManager =& Services::getService("Shared");
+// 
+// $qualifierHierarchyId =& $sharedManager->getId("673");
+// $functionId =& $sharedManager->createId();
+// $functionType =& new HarmoniType("Concerto", "Midd", "Use", "Functions for viewing and using shiznat.");
+// $authZManager->createFunction($functionId, "Comment", "Add comments to this thing.", $functionType, $qualifierHierarchyId);
+// exit;
+
+
+
+
+// Get the function type and print this info in two headers.
+$functionTypes =& $authZManager->getFunctionTypes();
+while ($functionTypes->hasNext()) {
+	$functionType =& $functionTypes->next();
+	ob_start();
+	print "<strong>".$functionType->getAuthority()." :: ";
+	print $functionType->getDomain()." :: ";
+	print $functionType->getKeyword()."</strong> ";
+	$typeLayout =& new SingleContentLayout(TEXT_BLOCK_WIDGET, 1);
+	$typeLayout->addComponent(new Content(ob_get_contents()));
+	ob_end_clean();
+	
+	ob_start();
+	print "<em>".$functionType->getDescription()."</em>";
+	$descriptionLayout =& new SingleContentLayout(TEXT_BLOCK_WIDGET, 2);
+	$descriptionLayout->addComponent(new Content(ob_get_contents()));
+	ob_end_clean();
+	
+	$actionRows->addComponent($typeLayout);
+	$actionRows->addComponent($descriptionLayout);
+	
+	$functionLayout =& new RowLayout(TEXT_BLOCK_WIDGET, 3);
+	$actionRows->addComponent($functionLayout);
+	
+	$actionRows->addComponent(new Content(" &nbsp; <br /> &nbsp; <br /> &nbsp;"));
+	
+// 	Now get all the functions of the specified type
+	$functions =& $authZManager->getFunctions($functionType);
+	while ($functions->hasNext()) {
+		$function =& $functions->next();
+		
+	//	Store function info and create layouts.
+	//	Types consist of {domain :: authority :: type :: description}
+	//	Functions consist of {Id :: displayName :: description}
+		
+		ob_start();
+		$id =& $function->getId();
+		print $id->getIdString()." - ";
+		print "<strong>".$function->getReferenceName()." - ";
+		print "</strong>".$function->getDescription();
+		
+		$functionLayout->addComponent(new Content(ob_get_contents()));
+		ob_end_clean();
+        
+	}
+}
+
+
+
+// Return the main layout.
+return $mainScreen;
