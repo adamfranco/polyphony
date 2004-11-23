d5cb8933e0da5caa4749369fe1b4097673186833
rrichards <rrichards>
2004-11-23 14:45:57 +0000

    *** empty log message ***

--- main/modules/authorization/process_authorizations.act.php
+++ main/modules/authorization/process_authorizations.act.php
@@ -7,20 +7,34 @@
 * copyright 2004 MIddlebury College
 */
 
-
-
+// Get services
 $shared =& Services::getService("Shared");
+$authZ =& Services::getService("AuthZ");
 
-printpre($_REQUEST);
-printpre($harmoni->pathInfoparts);
+// Get info passed to this action via the URL
+$createOrDelete = $harmoni->pathInfoParts['2'];
+$agentIdString = $harmoni->pathInfoParts['3'];
+$functionIdString = $harmoni->pathInfoParts['4'];
+$qualifierIdString = $harmoni->pathInfoParts['5'];
+
+// Get Ids from these strings
+$agentId =& $shared->getId($agentIdString);
+$functionId =& $shared->getId($functionIdString);
+$qualifierId =& $shared->getId($qualifierIdString);
+
 
 // Process authorizations
+if ($createOrDelete == 'create') {
+	$authZ->createAuthorization($agentId, $functionId, $qualifierId);
 
+} else {
+	$authorizations =& $authZ->getExplicitAZs($agentId, $functionId, $qualifierId, false);
+	while ($authorizations->hasNext()) {
+		$authorization =& $authorizations->next();
+		$authZ->deleteAuthorization($authorization);
 
-
-
-
-
+	}
+}
 
 
 // Send us back to where we were (edit_authorizations.act.php)
