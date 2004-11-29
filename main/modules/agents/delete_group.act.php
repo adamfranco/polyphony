43d5b66681bcee01101c37162703e90c5daac946
rrichards <rrichards>
2004-11-29 22:40:17 +0000

    Deletes a specified group.

---
+++ main/modules/agents/delete_group.act.php
@@ -0,0 +1,31 @@
+<?
+
+/**
+* delete_group.act.php
+* This action will delete the group as specified by add_delete_group.act.php
+* 11/29/04 Ryan Richards
+* copyright 2004 MIddlebury College
+*/
+
+// Get services
+$shared =& Services::getService("Shared");
+$authZ =& Services::getService("AuthZ");
+
+// Get info passed to this action via the URL
+$idString = $harmoni->pathInfoParts['2'];
+
+// Get the groupId from the Id string
+//$groupId = $shared->getId($idString);
+
+
+// Send us back to where we were (add_delete_group.php)
+$currentPathInfo = array_slice($harmoni->pathInfoParts, 3);
+header("Location: ".MYURL."/".implode("/",$currentPathInfo));
+
+
+
+// Delete the given group
+//$shared->deleteGroup($groupId);
+
+
+
