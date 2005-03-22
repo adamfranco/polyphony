a6c6df7e1fa3552aaf7e8805518033ab04936a68
adamfranco <adamfranco>
2005-03-22 22:25:36 +0000

    Adding a basic action for an authentication form.

---
+++ main/modules/auth/username_password_form.act.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package polyphony.modules.authentication
+ */
+
+// Get the Layout compontents. See core/modules/moduleStructure.txt
+// for more info. 
+$harmoni->ActionHandler->execute("window", "screen");
+$mainScreen =& $harmoni->getAttachedData('mainScreen');
+$statusBar =& $harmoni->getAttachedData('statusBar');
+$centerPane =& $harmoni->getAttachedData('centerPane');
+
+
+// Get info to send back to where we were on login
+$currentPathInfo = array();
+for ($i = 2; $i < count($harmoni->pathInfoParts); $i++) {
+	$currentPathInfo[] = $harmoni->pathInfoParts[$i];
+} 
+
+// Set our textdomain
+$defaultTextDomain = textdomain();
+textdomain("polyphony");
+ob_start();
+
+// text
+$introText =& new SingleContentLayout(TEXT_BLOCK_WIDGET, 2);
+
+$action = MYURL."/".implode("/",array_slice($harmoni->pathInfoParts, 2));
+$usernameText = _("Username");
+$passwordText = _("Password");
+print<<<END
+
+<form name='login' action='$action' method='post'>
+	$usernameText: <input type='text' name='username' />
+	<br />$passwordText: <input type='password' name='password' />
+	<br /><input type='submit' />
+</form>
+
+END;
+
+$introText->addComponent(new Content(ob_get_contents()));
+ob_end_clean();
+$centerPane->addComponent($introText, TOP, CENTER);
+
+// go back to the default text domain
+textdomain($defaultTextDomain);
+
+// return the main layout.
+return $mainScreen;
