e758c8eddf77bc560954ad536fbb16f5f122cf1a
cws-midd <cws-midd>
2005-09-28 19:15:04 +0000

    new config files for polyphony

---
+++ config/domit_default.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Set up the DOMIT location for XML handling
+ *
+ * USAGE: Copy this file to domit.conf.php to set custom values.
+ *
+ * @package polyphony.config
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: domit_default.conf.php,v 1.1 2005/09/28 19:15:04 cws-midd Exp $
+ */
+ 
+if (!defined("DOMIT"))
+	define("DOMIT", POLYPHONY."../domit/xml_domit_include.php");
---
+++ config/exif_default.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Set up the EXIF location for EXIF metadata handling
+ *
+ * USAGE: Copy this file to exif.conf.php to set custom values.
+ *
+ * @package polyphony.config
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: exif_default.conf.php,v 1.1 2005/09/28 19:15:04 cws-midd Exp $
+ */
+ 
+if (!defined("EXIF"))
+	define("EXIF", POLYPHONY."../PHP_JPEG_Metadata_Toolkit_1.11/EXIF.php");
