627adacde7f0990592f6628b67f1f40e8b8b697d
adamfranco <adamfranco>
2006-03-07 18:02:14 +0000

    *** empty log message ***

--- main/modules/logs/browse.act.php
+++ main/modules/logs/browse.act.php
@@ -6,7 +6,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: browse.act.php,v 1.3 2006/03/07 15:32:31 adamfranco Exp $
+ * @version $Id: browse.act.php,v 1.4 2006/03/07 18:02:14 adamfranco Exp $
  */ 
 
 require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php");
@@ -22,7 +22,7 @@ require_once(HARMONI."GUIManager/Components/Blank.class.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: browse.act.php,v 1.3 2006/03/07 15:32:31 adamfranco Exp $
+ * @version $Id: browse.act.php,v 1.4 2006/03/07 18:02:14 adamfranco Exp $
  */
 class browseAction 
 	extends MainWindowAction
@@ -127,125 +127,126 @@ class browseAction
 		}
 		$actionRows->add(new Block(ob_get_clean(), STANDARD_BLOCK), "100%", null, LEFT, TOP);
 		
-		
-		$log =& $loggingManager->getLogForReading($currentLogName);
-		$actionRows->add(new Heading($log->getDisplayName(), 3), "100%", null, LEFT, CENTER);
-		ob_start();
-		
-		
-		// Links to other priorities
-		print "<strong>"._("Priority: ")."</strong>";
-		if (RequestContext::value("priority"))
-			$currentPriorityType =& Type::stringToType(
-										RequestContext::value("priority"));
+		if (isset($currentLogName)) {
+			$log =& $loggingManager->getLogForReading($currentLogName);
+			$actionRows->add(new Heading($log->getDisplayName(), 3), "100%", null, LEFT, CENTER);
+			ob_start();
 			
-		$priorityTypes =& $loggingManager->getPriorityTypes();
-		while ($priorityTypes->hasNext()) {
-			$priorityType =& $priorityTypes->next();
-							
-			if (!isset($currentPriorityType))
-				$currentPriorityType =& $priorityType;
 			
-			if (!$priorityType->isEqual($currentPriorityType)) {
-				print "\n<a href='";
-				print $harmoni->request->quickURL("logs", "browse",
-						array(	"log" => RequestContext::value("log"),
-								"priority" => Type::typeToString($priorityType)));
-				print "'>".$priorityType->getKeyword()."</a>";
-			} else
-				print $priorityType->getKeyword();
+			// Links to other priorities
+			print "<strong>"._("Priority: ")."</strong>";
+			if (RequestContext::value("priority"))
+				$currentPriorityType =& Type::stringToType(
+											RequestContext::value("priority"));
+				
+			$priorityTypes =& $loggingManager->getPriorityTypes();
+			while ($priorityTypes->hasNext()) {
+				$priorityType =& $priorityTypes->next();
+								
+				if (!isset($currentPriorityType))
+					$currentPriorityType =& $priorityType;
+				
+				if (!$priorityType->isEqual($currentPriorityType)) {
+					print "\n<a href='";
+					print $harmoni->request->quickURL("logs", "browse",
+							array(	"log" => RequestContext::value("log"),
+									"priority" => Type::typeToString($priorityType)));
+					print "'>".$priorityType->getKeyword()."</a>";
+				} else
+					print $priorityType->getKeyword();
+				
+				if ($priorityTypes->hasNext())
+					print " | ";
+			}
 			
-			if ($priorityTypes->hasNext())
-				print " | ";
-		}
-		
-		
-		// Entries
-		print<<<END
-
-<script type='text/javascript'>
-	/* <![CDATA[ */
-
-	function showTrace(buttonElement) {
-		newWindow = window.open("", "traceWindow", 'toolbar=no,width=600,height=500,resizable=yes,scrollbars=yes,status=no')
-		// the next sibling is text, the one after that is our hidden div
-		newWindow.document.write(buttonElement.nextSibling.nextSibling.innerHTML)
-		newWindow.document.bgColor="lightpink"
-		newWindow.document.close() 
-	}
-
-	/* ]]> */
-</script>
-
-<table border='1'>
-<tr>
-	<th></th>
-	<th>timestamp</th>
-	<th>priority</th>
-	<th>description</th>
-	<th>trace</th>
-	<th>agents</th>
-	<th>nodes</th>
-</tr>
-
-END;
+			
+			// Entries
+			print<<<END
 	
-		$entries =& $log->getEntries($formatType, $currentPriorityType);
-		$i = $entries->count();
-		while ($entries->hasNext()) {
-			$entry =& $entries->next();
-			print "\n\t<tr>";
-			
-			print "\n\t\t<td style='text-align: right'>$i</td>";
-			$i--;
-			
-			$timestamp =& $entry->getTimestamp();
-			print "\n\t\t<td>".$timestamp->asString()."</td>";
-			
-			$priority =& $entry->getPriorityType();
-			print "\n\t\t<td>".$priority->getKeyword()."</td>";
-			
-			$item =& $entry->getItem();
-			
-			print "\n\t\t<td>".$item->getDescription()."</td>";
-			
-			print "\n\t\t<td>";
-			if ($trace = $item->getBacktrace()) {
-				print "\n\t\t\t<input type='button' value='"._("Show Trace")."' onclick='showTrace(this)'/>";
-				print "\n\t\t\t<div style='display: none'>".$trace."</div>";
-			}
-			print "</td>";
-			
-			print "\n\t\t<td>";
-			$agentIds =& $item->getAgentIds(true);
-			while ($agentIds->hasNext()) {
-				$agent =& $agentManager->getAgent($agentIds->next());
-				print $agent->getDisplayName();
-				if ($agentIds->hasNext())
-					print ", ";
-			}
-			print "\n\t\t</td>";
-			
-			print "\n\t\t<td>";
-			$nodeIds =& $item->getNodeIds(true);
-			while ($nodeIds->hasNext()) {
-				$nodeId =& $nodeIds->next();
-				if ($hierarchyManager->nodeExists($nodeId)) {
-					$node =& $hierarchyManager->getNode($nodeId);
-					print $node->getDisplayName();
-				} else {
-					print $nodeId->getIdString();
-				}
-				if ($nodeIds->hasNext())
-					print ", <br/>";
-			}
-			print "\n\t\t</td>";
-			
-			print "\n\t</tr>";
+	<script type='text/javascript'>
+		/* <![CDATA[ */
+	
+		function showTrace(buttonElement) {
+			newWindow = window.open("", "traceWindow", 'toolbar=no,width=600,height=500,resizable=yes,scrollbars=yes,status=no')
+			// the next sibling is text, the one after that is our hidden div
+			newWindow.document.write(buttonElement.nextSibling.nextSibling.innerHTML)
+			newWindow.document.bgColor="lightpink"
+			newWindow.document.close() 
 		}
+	
+		/* ]]> */
+	</script>
+	
+	<table border='1'>
+	<tr>
+		<th></th>
+		<th>timestamp</th>
+		<th>priority</th>
+		<th>description</th>
+		<th>trace</th>
+		<th>agents</th>
+		<th>nodes</th>
+	</tr>
+	
+END;
 		
-		print "\n</table>";
-		$actionRows->add(new Block(ob_get_clean(), STANDARD_BLOCK), "100%", null, LEFT, TOP);
+			$entries =& $log->getEntries($formatType, $currentPriorityType);
+			$i = $entries->count();
+			while ($entries->hasNext()) {
+				$entry =& $entries->next();
+				print "\n\t<tr>";
+				
+				print "\n\t\t<td style='text-align: right'>$i</td>";
+				$i--;
+				
+				$timestamp =& $entry->getTimestamp();
+				print "\n\t\t<td>".$timestamp->asString()."</td>";
+				
+				$priority =& $entry->getPriorityType();
+				print "\n\t\t<td>".$priority->getKeyword()."</td>";
+				
+				$item =& $entry->getItem();
+				
+				print "\n\t\t<td>".$item->getDescription()."</td>";
+				
+				print "\n\t\t<td>";
+				if ($trace = $item->getBacktrace()) {
+					print "\n\t\t\t<input type='button' value='"._("Show Trace")."' onclick='showTrace(this)'/>";
+					print "\n\t\t\t<div style='display: none'>".$trace."</div>";
+				}
+				print "</td>";
+				
+				print "\n\t\t<td>";
+				$agentIds =& $item->getAgentIds(true);
+				while ($agentIds->hasNext()) {
+					$agent =& $agentManager->getAgent($agentIds->next());
+					print $agent->getDisplayName();
+					if ($agentIds->hasNext())
+						print ", ";
+				}
+				print "\n\t\t</td>";
+				
+				print "\n\t\t<td>";
+				$nodeIds =& $item->getNodeIds(true);
+				while ($nodeIds->hasNext()) {
+					$nodeId =& $nodeIds->next();
+					if ($hierarchyManager->nodeExists($nodeId)) {
+						$node =& $hierarchyManager->getNode($nodeId);
+						print $node->getDisplayName();
+					} else {
+						print $nodeId->getIdString();
+					}
+					if ($nodeIds->hasNext())
+						print ", <br/>";
+				}
+				print "\n\t\t</td>";
+				
+				print "\n\t</tr>";
+			}
+			
+			print "\n</table>";
+			$actionRows->add(new Block(ob_get_clean(), STANDARD_BLOCK), "100%", null, LEFT, TOP);
+		}
  		
  		textdomain($defaultTextDomain);
 	}
