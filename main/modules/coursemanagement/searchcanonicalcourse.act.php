e752fb28d34e2fb62cbce9ca11a352331fe1e999
jwlee100 <jwlee100>
2006-07-19 19:07:00 +0000

    *** empty log message ***

--- main/modules/coursemanagement/searchcanonicalcourse.act.php
+++ main/modules/coursemanagement/searchcanonicalcourse.act.php
@@ -6,12 +6,11 @@
  * @copyright Copyright &copy; 2006, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: searchcanonicalcourse.act.php,v 1.5 2006/07/18 21:02:22 jwlee100 Exp $
+ * @version $Id: searchcanonicalcourse.act.php,v 1.6 2006/07/19 19:07:00 jwlee100 Exp $
  */ 
 
 require_once(POLYPHONY."/main/library/AbstractActions/MainWindowAction.class.php");
 require_once(HARMONI."GUIManager/Components/Blank.class.php");
-
 
 
 class searchcanonicalcourseAction 
@@ -29,8 +28,8 @@ class searchcanonicalcourseAction
  		$authZManager =& Services::getService("AuthZ");
  		$idManager =& Services::getService("IdManager");
  		if ($authZManager->isUserAuthorized(
- 			$idManager->getId("edu.middlebury.authorization.view"),
- 			$idManager->getId("edu.middlebury.coursemanagement")))
+ 					$idManager->getId("edu.middlebury.authorization.view"),
+ 					$idManager->getId("edu.middlebury.coursemanagement")))
  		{
 			return TRUE;
  		} else {
@@ -47,7 +46,7 @@ class searchcanonicalcourseAction
 	 * @since 4/26/05
 	 */
 	function getHeadingText () {
-		return dgettext("polyphony", "Refresh Canonical Courses by Term");
+		return dgettext("polyphony", "Search Canonical Courses by Term");
 	}
 	
 	/**
@@ -58,40 +57,22 @@ class searchcanonicalcourseAction
 	 * @since 4/26/05
 	 */
 	function buildContent () {
+	
 		$actionRows =& $this->getActionRows();
 		$pageRows =& new Container(new YLayout(), OTHER, 1);
 		$harmoni =& Harmoni::instance();
 		
-		$harmoni->request->startNamespace("polyphony-agents");
-
-		$agentManager =& Services::getService("Agent");
-		$idManager = Services::getService("Id");
 		$cmm =& Services::getService("CourseManagement");
-		
-		
-		
-		
-		
-		/*********************************************************
-		 * the select menu
-		 *********************************************************/
-		// Users header
-		//$actionRows->add(new Heading(_("Select Term"), 2), "100%", null, LEFT, CENTER);
 		
 		ob_start();
 		$self = $harmoni->request->quickURL();
-		
-		$lastTerm = $harmoni->request->get("term_name");
-		$term_name = RequestContext::name("term_name");
-		//$search_type_name = RequestContext::name("search_type");
-		print _("<p align='center'>Select a Term").": </p>";
-		print <<<END
-		<form action='$self' method='post'>
+		print ("<p align='center'>Search for a canonical course by the following criteria").": </p>";
+		print "<form action='$self' method='post'>
 			<div>
-			<input type='text' name='search_title'>
-			<input type='text' name='search_number'>
-			<select name='search_type'>
-		END;
+			Title: <input type='text' name='search_title'>
+			Number: <input type='text' name='search_number'>";
+		/*
+		print "<select name='search_type'>";
 		
 		$courseTypes =& $cmm->getCanonicalCourseTypes();
 	
@@ -101,17 +82,15 @@ class searchcanonicalcourseAction
 			$id =& $type->getId();
 			$idString = $id->getIdString();
 			print "\n\t\t<option value='".$idString."'";
-			if ($harmoni->request->get('type') == $idString) {
+			if ($harmoni->request->get('type') == $idString)
 				print " selected='selected'";
-			}
+			
 			print ">".$type->getDisplayName()."</option>";
 		}
 		
 		print "\n\t</select>";
 		
-		print <<<END
-			<select name='search_status'>
-		END;
+		print "<select name='search_status'>";
 		
 		$courseStatusTypes =& $cmm->getCanonicalCourseTypes();
 	
@@ -121,83 +100,78 @@ class searchcanonicalcourseAction
 			$id =& $status->getId();
 			$idString = $id->getIdString();
 			print "\n\t\t<option value='".$idString."'";
-			if ($harmoni->request->get('type') == $idString) {
+			if ($harmoni->request->get('type') == $idString)
 				print " selected='selected'";
-			}
+			
 			print ">".$status->getKeyword()."</option>";
 		}
 		
 		print "\n\t</select>";
-		
-		print "\n\t<input type='submit' value='"._("Suck!")."' />";
+		*/
+		print "\n\t<input type='submit' value='"._("Search!")."' />";
 		//print "\n\t<a href='".$harmoni->request->quickURL()."'>";
+		
 		print "\n</p>\n</div></form>";
-		print "\n  <p align='center'>Sucking may take a few minutes</p>";
+		print "\n  <p align='center'>Search may take a few minutes</p>";
 		
-		ob_end_clean();
 		$actionRows->add(new Block(ob_get_contents(), STANDARD_BLOCK), "100%", null, LEFT, CENTER);
+		ob_end_clean();
 		
-		/*********************************************************
-		 * Canonical course search results
-		 *********************************************************/
-		$pageRows->add(new Heading(_("Canonical course search results ".$term->getDisplayName().""), 2), "100%", null,
-		 			   LEFT, CENTER);
+		$searchTitle = RequestContext::value('search_title');
+		$searchNumber = RequestContext::value('search_number');
+		$searchType = RequestContext::value('search_type');
+		$searchStatus = RequestContext::value('search_status');
+		
+		$pageRows->add(new Heading("Canonical course search results", STANDARD_BLOCK), "100%", null, LEFT, CENTER);
+			
 		ob_start();
-		if ($searchTitle = RequestContext::value('search_title') ||	
-			$searchNumber = RequestContext::value('search_number') ||
-			$searchType = RequestContext::value('search_type') ||
-			$searchStatus = RequestContext::value('search_status')) {
 					
-			print "\n<table border=1>";
-			print "\n\t<tr>";
-			print "\n\t<td>";
-			print "<b>Title</b>";
-			print "\n\t<td>";
-			print "<b>Number</b>";
-			print "\n\t<td>";
-			print "<b>Description</b>";
-			print "\n\t<td>";
-			print "<b>Course Type</b>";
-			print "\n\t<td>";
-			print "<b>Course Status</b>";
-			print "\n\t<td>";
-			print "<b>Credits</b>";
-			print "\n\t</tr>";
-			$canonicalCourseIterator = $courseManager->getCanonicalCourses();
-			while ($canonicalCourseIterator->hasNext()) {
-				$canonicalCourse = $canonicalCourseIterator->next();
-				$title = $canonicalCourse->getTitle();
-	  			$number = $canonicalCourse->getNumber();
-	  			$courseType = $canonicalCourse->getCourseType();
-	  			$courseKeyword = $courseType->getKeyword();
-	  			$courseStatusType = $canonicalCourse->getStatus();
-	  			$courseStatusKeyword = $courseStatusType->getKeyword();
-				if ($searchTitle == $title || $searchNumber == $number ||
-					$searchType == $courseKeyword || $searchStatus == $courseStatusKeyword) {
-					$description = $canonicalCourse->getDescription();
-					$credits = $canonicalCourse->getCredits();
+		print "\n<table border=1>";
+		print "\n\t<tr>";
+		print "\n\t<td>";
+		print "<b>Title</b>";
+		print "\n\t<td>";
+		print "<b>Number</b>";
+		print "\n\t<td>";
+		print "<b>Description</b>";
+		print "\n\t<td>";
+		print "<b>Course Type</b>";
+		print "\n\t<td>";
+		print "<b>Course Status</b>";
+		print "\n\t<td>";
+		print "<b>Credits</b>";
+		print "\n\t</tr>";
+		$canonicalCourseIterator = $cmm->getCanonicalCourses();
+		while ($canonicalCourseIterator->hasNext()) {
+			$canonicalCourse = $canonicalCourseIterator->next();
+			$title = $canonicalCourse->getTitle();
+			$number = $canonicalCourse->getNumber();
+			$courseType = $canonicalCourse->getCourseType();
+			$courseKeyword = $courseType->getKeyword();
+			$courseStatusType = $canonicalCourse->getStatus();
+			$courseStatusKeyword = $courseStatusType->getKeyword();
+			if ($searchTitle == $title || $searchNumber == $number ||
+				$searchType == $courseKeyword || $searchStatus == $courseStatusKeyword) {
+				  
+				$description = $canonicalCourse->getDescription();
+				$credits = $canonicalCourse->getCredits();
 				
-					print "<tr>";
-					print "<td>";
-					print $title;
-					print "<td>";
-					print $number;
-					print "<td>";
-					print $description;
-					print "<td>";
-					print $courseKeyword;
-					print "<td>";
-					print $courseStatusKeyword;
-					print "<td>";
-					print $credits;
-					print "</tr>";
-				}
+				print "<tr>";
+				print "<td>";
+				print $title;
+				print "<td>";
+				print $number;
+				print "<td>";
+				print $description;
+				print "<td>";
+				print $courseKeyword;
+				print "<td>";
+				print $courseStatusKeyword;
+				print "<td>";
+				print $credits;
+				print "</tr>";
 			}
 		}
-
-		print "Success!";
-	
-		// Create a layout for this group using the GroupPrinter
 		
 		$groupLayout =& new Block(ob_get_contents(), STANDARD_BLOCK);
 		ob_end_clean();
@@ -206,4 +180,3 @@ class searchcanonicalcourseAction
 		$actionRows->add($pageRows, "100%", null, LEFT, CENTER);	
 	}
 }
-?>
