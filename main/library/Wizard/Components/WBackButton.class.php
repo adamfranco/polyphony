036a5f867ebed52a03ab74e7e7a59e125c32c524
sporktim <sporktim>
2006-08-15 21:00:51 +0000

    Navigation of the logic wizard is greatly aided by forward and and back buttons.  Validation with them is not fully tested, though.

---
+++ main/library/Wizard/Components/WBackButton.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @since Jul 20, 2005
+ * @package polyphony.library.wizard.components
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: WBackButton.class.php,v 1.1 2006/08/15 21:00:51 sporktim Exp $
+ */ 
+
+require_once(POLYPHONY."/main/library/Wizard/Components/WEventButton.class.php");
+
+/**
+ * This adds a "Back" button to the wizard and throws the appropriate event.
+ * 
+ * @since Jul 20, 2005
+ * @package polyphony.library.wizard.components
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: WBackButton.class.php,v 1.1 2006/08/15 21:00:51 sporktim Exp $
+ */
+class WBackButton extends WEventButton {
+	
+	var $_stepContainer;
+
+	/**
+	 * Constructor
+	 * @param ref object $stepContainer A {@link WizardStepContainer} object.
+	 * @access public
+	 * @return void
+	 */
+	function WBackButton (&$stepContainer) {
+		$this->setLabel(_("<-- Back"));
+		$this->_stepContainer =& $stepContainer;
+		$this->addOnClick("ignoreValidation(this.form);");
+	}
+
+	/**
+	 * Tells the wizard component to update itself - this may include getting
+	 * form post data or validation - whatever this particular component wants to
+	 * do every pageload. 
+	 * @param string $fieldName The field name to use when outputting form data or
+	 * similar parameters/information.
+	 * @access public
+	 * @return boolean - TRUE if everything is OK
+	 */
+	function update ($fieldName) {
+		parent::update($fieldName);
+		if ($this->getAllValues()) {
+			// go back!
+			$this->_stepContainer->goBack();
+		}
+	}
+	
+	/**
+	 * Answers true if this component will be enabled.
+	 * @access public
+	 * @return boolean
+	 */
+	function isEnabled () {
+		return $this->_stepContainer->canGoBack();
+	}
+
+}
+
+?>
---
+++ main/library/Wizard/Components/WForwardButton.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @since Jul 20, 2005
+ * @package polyphony.library.wizard.components
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: WForwardButton.class.php,v 1.1 2006/08/15 21:00:51 sporktim Exp $
+ */ 
+
+require_once(POLYPHONY."/main/library/Wizard/Components/WEventButton.class.php");
+
+/**
+ * This adds a "Back" button to the wizard and throws the appropriate event.
+ * 
+ * @since Jul 20, 2005
+ * @package polyphony.library.wizard.components
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: WForwardButton.class.php,v 1.1 2006/08/15 21:00:51 sporktim Exp $
+ */
+class WForwardButton extends WEventButton {
+	
+	var $_stepContainer;
+
+	/**
+	 * Constructor
+	 * @param ref object $stepContainer A {@link WizardStepContainer} object.
+	 * @access public
+	 * @return void
+	 */
+	function WForwardButton (&$stepContainer) {
+		$this->setLabel(_("Forward -->"));
+		$this->_stepContainer =& $stepContainer;
+	}
+
+	/**
+	 * Tells the wizard component to update itself - this may include getting
+	 * form post data or validation - whatever this particular component wants to
+	 * do every pageload. 
+	 * @param string $fieldName The field name to use when outputting form data or
+	 * similar parameters/information.
+	 * @access public
+	 * @return boolean - TRUE if everything is OK
+	 */
+	function update ($fieldName) {
+		parent::update($fieldName);
+		$wizard =& $this->getWizard();
+		if ($this->getAllValues()) {
+			// go forward!
+			$this->_stepContainer->goForward();
+		}
+	}
+	
+	/**
+	 * Answers true if this component will be enabled.
+	 * @access public
+	 * @return boolean
+	 */
+	function isEnabled () {
+		return $this->_stepContainer->canGoForward();
+	}
+
+}
+
+?>
