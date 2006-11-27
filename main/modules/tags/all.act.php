88576a9ee4aa9a8ddfbe55b9fa58059835c718b6
adamfranco <adamfranco>
2006-11-27 23:02:55 +0000

    Removing debugging.

--- main/modules/tags/all.act.php
+++ main/modules/tags/all.act.php
@@ -6,7 +6,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: all.act.php,v 1.1.2.2 2006/11/08 21:59:34 adamfranco Exp $
+ * @version $Id: all.act.php,v 1.1.2.3 2006/11/27 23:02:55 adamfranco Exp $
  */ 
 
 require_once(dirname(__FILE__)."/TagAction.abstract.php");
@@ -20,7 +20,7 @@ require_once(dirname(__FILE__)."/TagAction.abstract.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: all.act.php,v 1.1.2.2 2006/11/08 21:59:34 adamfranco Exp $
+ * @version $Id: all.act.php,v 1.1.2.3 2006/11/27 23:02:55 adamfranco Exp $
  */
 class allAction 
 	extends TagAction
@@ -62,46 +62,46 @@ class allAction
 		return $tags;
 	}
 	
-	/**
-	 * This just adds an agent list for debugging purposes.
-	 * 
-	 * @return void
-	 * @access public
-	 * @since 11/07/06
-	 */
-	function buildContent () {
-		parent::buildContent();
-		
-		$defaultTextDomain = textdomain("polyphony");
-		$actionRows =& $this->getActionRows();
-		ob_start();
-		$harmoni =& Harmoni::instance();
-		$harmoni->request->startNamespace("polyphony-tags");
-		
-		print "\n\t<h3>Debug: Top Agents</h3>";
-		$tagManager =& Services::getService("Tagging");
-		$agentManager =& Services::getService('Agent');
-		$agentIds =& $tagManager->getAgentIds();
-		$i=0;
-		while ($agentIds->hasNext() && $i < 10) {
-			$agentId =& $agentIds->next();
-			
-			if ($agentManager->isAgent($agentId)) {
-				$agent =& $agentManager->getAgent($agentId);
-				$name = $agent->getDisplayName();
-			} else
-				$name = $agentId->getIdString();
-			
-			$url = $harmoni->request->quickUrl('tags', 'user', 
-				array('agent_id' => $agentId->getIdString()));
-			print "\n\t<a href='".$url."'>".$name."</a> ";
-		}
-		
-		
-		$actionRows->add(new Block(ob_get_clean(), HIGHLIT_BLOCK), "100%", null, LEFT, TOP);
-		$harmoni->request->endNamespace();
-		textdomain($defaultTextDomain);
-	}
+// 	/**
+// 	 * This just adds an agent list for debugging purposes.
+// 	 * 
+// 	 * @return void
+// 	 * @access public
+// 	 * @since 11/07/06
+// 	 */
+// 	function buildContent () {
+// 		parent::buildContent();
+// 		
+// 		$defaultTextDomain = textdomain("polyphony");
+// 		$actionRows =& $this->getActionRows();
+// 		ob_start();
+// 		$harmoni =& Harmoni::instance();
+// 		$harmoni->request->startNamespace("polyphony-tags");
+// 		
+// 		print "\n\t<h3>Debug: Top Agents</h3>";
+// 		$tagManager =& Services::getService("Tagging");
+// 		$agentManager =& Services::getService('Agent');
+// 		$agentIds =& $tagManager->getAgentIds();
+// 		$i=0;
+// 		while ($agentIds->hasNext() && $i < 10) {
+// 			$agentId =& $agentIds->next();
+// 			
+// 			if ($agentManager->isAgent($agentId)) {
+// 				$agent =& $agentManager->getAgent($agentId);
+// 				$name = $agent->getDisplayName();
+// 			} else
+// 				$name = $agentId->getIdString();
+// 			
+// 			$url = $harmoni->request->quickUrl('tags', 'user', 
+// 				array('agent_id' => $agentId->getIdString()));
+// 			print "\n\t<a href='".$url."'>".$name."</a> ";
+// 		}
+// 		
+// 		
+// 		$actionRows->add(new Block(ob_get_clean(), HIGHLIT_BLOCK), "100%", null, LEFT, TOP);
+// 		$harmoni->request->endNamespace();
+// 		textdomain($defaultTextDomain);
+// 	}
 	
 	/**
 	 * Answer the action to use for viewing tags
