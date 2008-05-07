1c8dff41a6b8455ab501c2f72d870160d1fe49f3
Adam Franco <afranco@middlebury.edu>
2008-05-07 13:39:59 -0400

    New action for listing themes.

---
+++ main/modules/gui2/theme_list.act.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @since 5/7/08
+ * @package polyphony.gui2
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */ 
+
+require_once(POLYPHONY.'/main/library/AbstractActions/MainWindowAction.class.php');
+
+/**
+ * Display a list of available themes.
+ * 
+ * @since 5/7/08
+ * @package polyphony.gui2
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */
+class theme_listAction
+	extends MainWindowAction
+{
+		
+	/**
+	 * AuthZ
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 5/6/08
+	 */
+	public function isAuthorizedToExecute () {
+		return true;
+	}
+	
+	/**
+	 * Execute
+	 * 
+	 * @return null
+	 * @access public
+	 * @since 5/7/08
+	 */
+	public function buildContent () {
+		$rows = $this->getActionRows();
+		$rows->add(new Heading(_("Themes"), 1));
+		
+		$guiMgr = Services::getService('GUIManager');
+		if (!method_exists($guiMgr, 'getThemeSources'))
+			throw new Exception('This action only works with Gui2.');
+		
+		$themes = $guiMgr->getThemes();
+		
+		$resultPrinter = new ArrayResultPrinter($themes, 1, 10, array($this, 'getThemeListing'));
+		$rows->add($resultPrinter->getLayout());
+	}
+	
+	/**
+	 * Answer a gui component for this theme listing
+	 * 
+	 * @param object Harmoni_Gui2_ThemeInterface $theme
+	 * @return object ComponentInterface
+	 * @access public
+	 * @since 5/7/08
+	 */
+	public function getThemeListing (Harmoni_Gui2_ThemeInterface $theme) {
+		ob_start();
+		try {
+			print "<h2>".$theme->getDisplayName()."</h2>";
+		} catch (UnimplementedException $e) {
+			print "<div style='font-style: italic'>"._("Display-Name not available.")."</div>";
+		}
+		try {
+			$thumb = $theme->getThumbnail();
+			$harmoni = Harmoni::instance();
+			print "<img src='".$harmoni->request->quickUrl('gui2', 'theme_thumbnail', array('theme' => $theme->getIdString()))."' style='float: left;'/>";
+		} catch (UnimplementedException $e) {
+			print "<div style='font-style: italic'>"._("Thumbnail not available.")."</div>";
+		}
+		print _("Id").": ".$theme->getIdString();
+		try {
+			print "<p>".$theme->getDescription()."</p>";
+		} catch (UnimplementedException $e) {
+			print "<div style='font-style: italic'>"._("Description not available.")."</div>";
+		}
+		return new Block(ob_get_clean(), STANDARD_BLOCK);
+		
+	}
+}
+
+?>
