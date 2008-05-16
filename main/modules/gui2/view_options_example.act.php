237477b7b36a98af4d883789e45a9a31553b7ae6
Adam Franco <afranco@middlebury.edu>
2008-05-16 16:51:38 -0400

    New actions to view the Theme Options schema and an example of theme options.

---
+++ main/modules/gui2/view_options_example.act.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @since 5/16/08
+ * @package polyphony.gui2
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */ 
+
+require_once(POLYPHONY.'/main/library/AbstractActions/Action.class.php');
+
+/**
+ * Display the Theme Options schema.
+ * 
+ * @since 5/16/08
+ * @package polyphony.gui2
+ * 
+ * @copyright Copyright &copy; 2007, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id$
+ */
+class view_options_exampleAction
+	extends Action
+{
+		
+	/**
+	 * AZ
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 5/16/08
+	 */
+	public function isAuthorizedToExecute () {
+		return true;
+	}
+	
+	/**
+	 * Execute
+	 * 
+	 * @return null
+	 * @access public
+	 * @since 5/16/08
+	 */
+	public function execute () {
+		header('Content-Type: text/xml');
+		print file_get_contents(MYDIR.'/themes-dist/RoundedCorners/options.xml');
+		exit;
+	}
+}
+
+?>
