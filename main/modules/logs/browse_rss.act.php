0650eea5fcc88f7b5bd82ae07595f23fd56ed281
Adam Franco <afranco@middlebury.edu>
2008-10-10 17:07:21 -0400

    Logs: Browsing of logs is now properly restricted to administrators.

--- main/modules/logs/browse_rss.act.php
+++ main/modules/logs/browse_rss.act.php
@@ -38,7 +38,7 @@ class browse_rssAction
  		$authZManager = Services::getService("AuthZ");
  		$idManager = Services::getService("IdManager");
  		if ($authZManager->isUserAuthorized(
- 					$idManager->getId("edu.middlebury.authorization.view"),
+ 					$idManager->getId("edu.middlebury.authorization.modify"),
  					$idManager->getId("edu.middlebury.authorization.root")))
  		{
 			return TRUE;
